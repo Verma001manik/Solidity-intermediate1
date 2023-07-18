453fcc47f3dd5736e0816e18e28b8adc2ef469ab
Manik Verma <97086386+Verma001manik@users.noreply.github.com>
2023-07-18 21:12:15 +0530

    Updated Simple.sol as per mentor

--- Simple.sol
+++ Simple.sol
@@ -2,37 +2,38 @@
 pragma solidity ^0.8.0;
 
 contract DepositWithdrawContract {
-    //public variable which has the totalamount //
-    uint256 public totalAmount;
+    int256 public totalAmount;
 
-    //deposit takes 1 arguement : amount 
-    function deposit(uint256 amount) external {
-        // Use require to validate the deposit amount
-        //amount>0 is the statement that is to be executed 
+    function deposit(int256 amount) external {
         require(amount > 0, "Deposit amount must be greater than zero");
-        //the other parameter is the one which gets logged when the 1st param is failed//
 
-        // Perform the deposit
+        // Use assert to check that totalAmount remains non-negative after the deposit
+        assert(totalAmount + amount >= totalAmount);
+
         totalAmount += amount;
-        //adds the amount to the totalamount
     }
-    // takes 1 param as deposit 
-    function withdraw(uint256 amount) external {
-        // Use require to check if there are enough funds to withdraw
+
+    function withdraw(int256 amount) external {
         require(amount <= totalAmount, "Insufficient funds");
 
-        // Use assert to check an internal invariant
-        //total amount should obviously be never less than 0 
-        assert(totalAmount >= 0);
+        // Use assert to check that totalAmount remains non-negative after the withdrawal
+        assert(totalAmount - amount >= 0);
 
-        // Perform the withdrawal
-        //withdrawing amount from totalAmount
         totalAmount -= amount;
+    }
+    uint public num=1;
+
+    function Asserting() public view {
+        
+        assert(num == 3);
+    }
+    function assertNonNegative(int256 value) public  pure {
+        assert(value >= 0);
     }
 
     function triggerRevert() external pure {
         // Use revert to explicitly trigger a revert with a custom message
         revert("This transaction explicitly reverted.");
-        //called when triggerRevert() is called 
     }
 }
+
